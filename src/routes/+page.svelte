e67ffdfc85b8af778bedd06469663924f02f8e36
rinjugatla <rin.jugatla@gmail.com>
2024-03-28 14:27:18 +0900

    イベントTweetをトップページに表示

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,10 @@
 	import { browser } from "$app/environment";
 
     const tweets = [
-        '<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">🌺BIG NEWS🌺<br><br>4/7(日) 18:30- <br>「第3回 <a href="https://twitter.com/hashtag/HibiscusCUP?src=hash&amp;ref_src=twsrc%5Etfw">#HibiscusCUP</a> inEFT」<br>開催決定ー!!🥳<br><br>今回は3種目<br>「謎解き」「フラッグ戦」「クイズ」👀！<br>６組１８名の<br>ストリーマーの皆様をご招待！<br><br>視聴者プレゼントの詳細は後日！<br>お楽しみに😉<a href="https://twitter.com/hashtag/%E3%82%BF%E3%83%AB%E3%82%B3%E3%83%95?src=hash&amp;ref_src=twsrc%5Etfw">#タルコフ</a>　<a href="https://twitter.com/hashtag/EscapefromTarkov?src=hash&amp;ref_src=twsrc%5Etfw">#EscapefromTarkov</a> <br><br>🎬詳細、ストリーマーの皆様はコチラ！ <a href="https://t.co/hmPldTNYVy">pic.twitter.com/hmPldTNYVy</a></p>&mdash; りえぺこ🌺:) @4/7 EFTイベ HibiscusCUP主催 (@Riepeko_yolo) <a href="https://twitter.com/Riepeko_yolo/status/1768570506708767059?ref_src=twsrc%5Etfw">March 15, 2024</a></blockquote>'
+        {
+            title: 'イベント概要',
+            contents: '<blockquote class="twitter-tweet"><p lang="ja" dir="ltr">🌺BIG NEWS🌺<br><br>4/7(日) 18:30- <br>「第3回 <a href="https://twitter.com/hashtag/HibiscusCUP?src=hash&amp;ref_src=twsrc%5Etfw">#HibiscusCUP</a> inEFT」<br>開催決定ー!!🥳<br><br>今回は3種目<br>「謎解き」「フラッグ戦」「クイズ」👀！<br>６組１８名の<br>ストリーマーの皆様をご招待！<br><br>視聴者プレゼントの詳細は後日！<br>お楽しみに😉<a href="https://twitter.com/hashtag/%E3%82%BF%E3%83%AB%E3%82%B3%E3%83%95?src=hash&amp;ref_src=twsrc%5Etfw">#タルコフ</a>　<a href="https://twitter.com/hashtag/EscapefromTarkov?src=hash&amp;ref_src=twsrc%5Etfw">#EscapefromTarkov</a> <br><br>🎬詳細、ストリーマーの皆様はコチラ！ <a href="https://t.co/hmPldTNYVy">pic.twitter.com/hmPldTNYVy</a></p>&mdash; りえぺこ🌺:) @4/7 EFTイベ HibiscusCUP主催 (@Riepeko_yolo) <a href="https://twitter.com/Riepeko_yolo/status/1768570506708767059?ref_src=twsrc%5Etfw">March 15, 2024</a></blockquote>'
+        }
     ];
 </script>
 
@@ -13,7 +16,9 @@
 
     {#each tweets as tweet}
     <div class="text-center flex justify-center">
-        {@html tweet} 
+        <div>
+            {@html tweet.contents}
+        </div>
     </div>
     {/each}
 </div>
