be50a1af53cf6a84d2b2b851b9e8fb47f23dea3e
rinjugatla <rin.jugatla@gmail.com>
2024-04-06 13:52:34 +0900

    サイトメタデータを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,11 @@ import Header from '$lib/components/commons/Header.svelte';
 	import '../app.pcss';
 </script>
 
+<svelte:head>
+	<title>HibiscusCUP inEFT 3rd fan</title>
+	<meta name="description" content="第三回HibiscusCUP inEFTのファンサイト" />
+</svelte:head>
+
 <div class="relative pb-14 min-h-screen h-full bg-hibiscus_cup_theme">
 	<Header />
 	<div class="my-5">
